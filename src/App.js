4211387a4599c0f64b0b5aacefca974dc2fa623d
Frank Veloz <velozmontero@gmail.com>
2019-11-24 13:54:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import * as ml5 from 'ml5';
+import ImageUploader from 'react-images-upload';
+import Img from 'react-image'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [images, setImages] = useState([]);
+
+  const classifyImage = async files => {
+    const src = window.URL.createObjectURL(files[files.length - 1]);
+    const classifier = await ml5.imageClassifier('MobileNet');
+
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.src = src;
+    img.width = 224;
+    img.height = 224;
+    img.onload = () => {
+      classifier.predict(img, 1, (err, results) => {
+        if (err) {
+          return console.error(err);
+        }
+
+        const [prediction] = results;
+        setImages(images => images.concat([{ file: files[0], src, prediction }]));
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <div className="images">
+          {images.map(({ src, prediction: { label, confidence } }, i) => (
+            <div key={i} className="image">
+              <Img
+                height={400}
+                src={[
+                  src,
+                  logo
+                ]}
+              />
+              <div>Label: {label}</div>
+              <div>Confidence: {Math.round(confidence * 100)}%</div>
+            </div>
+            
+          ))}
+        </div>
+
+        <div className="image-uploader">
+          <p>
+            ML5 Version {ml5.version}
+          </p>
+
+          <div style={{
+            width: '320px',
+            height: '50px',
+            color: '#282c34'
+          }}>
+            <ImageUploader
+              withIcon
+              singleImage
+              buttonText='Choose image'
+              onChange={classifyImage}
+              imgExtension={['.jpg', '.gif', '.png', '.gif']}
+              maxFileSize={5242880}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
